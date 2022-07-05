e4533dd1eac271d1b4c67becdadd40818fe4a309
Elshad Asafov <105041022+elshadasafov@users.noreply.github.com>
2022-07-05 11:53:23 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,19 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  let eded = parseInt(result.input);
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+  var result = false;
+  for (i = 2; i < eded; i++) {
+    if (eded % i === 0) {
+        result = true
+        console.log('No');
+        break;
+    }
+
+  }
+  if(result === false){
+    console.log('Yes');
+  }
 });
