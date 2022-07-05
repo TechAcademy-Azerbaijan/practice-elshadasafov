a8fbe425b10fbcb69a7518761e932aa90fe6f1cf
Elshad Asafov <105041022+elshadasafov@users.noreply.github.com>
2022-07-05 11:52:44 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,9 +1,12 @@
-const prompt = require('prompt');
+const prompt = require("prompt");
 prompt.start();
 
+prompt.get("input", function (err, result) {
+  let arr = result.input.split(',');
+  let arr2 = arr.reverse()
+  
+  for( i=0; i<arr2.length; i++){
+    console.log(arr2[i]);
+  }
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
 });
