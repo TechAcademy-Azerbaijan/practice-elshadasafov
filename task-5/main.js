9092f7949ee4a81a3991c847c6a33c73646d1cc5
Elshad Asafov <105041022+elshadasafov@users.noreply.github.com>
2022-07-08 10:02:30 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,13 @@
 const prompt = require('prompt');
 prompt.start();
 
-
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    let input = result.input; 
+    let numbers = input.split(','); 
+    let a = parseInt(numbers[0]); 
+    let b = parseInt(numbers[1]); 
+    let c = parseInt(numbers[2]); 
+    let d = parseInt(numbers[3]);
+
+    console.log(Math.max(a,b,c,d));
 });
